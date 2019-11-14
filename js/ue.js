e443195d6bd12c031c29e4b3b94bd667aeb1f869
JacobLinCool <jacoblin2011@gmail.com>
2019-11-14 16:32:06 +0800

    Update ue.js

--- js/ue.js
+++ js/ue.js
@@ -8,14 +8,20 @@ var ue = (function () {
             return new Promise(x=>{x(this.checked.instagram[n])});
         else
             return fetch("https://ue-instagram.1481.workers.dev/"+n)
-            .then(r=>r.json());
+            .then(r=>r.json()).then(res=>{
+                this.checked.instagram[n] = res;
+                return res;
+            });
     };
     var github = function(n) {
         if(this.checked.github[n] !== undefined)
             return new Promise(x=>{x(this.checked.github[n])});
         else
             return fetch("https://ue-github.1481.workers.dev/"+n)
-            .then(r=>r.json());
+            .then(r=>r.json()).then(res=>{
+                this.checked.github[n] = res;
+                return res;
+            });
     };
     return ({
         checked,
